278103a3aea8b9af7804286b4acbc86c9adcb306
ucbiyyq <yangyang.qian@ischool.berkeley.edu>
2019-03-27 04:27:26 -0400

    tweaked setup.py

--- setup.py
+++ setup.py
@@ -6,5 +6,7 @@ setup(
     version='0.1.0',
     description='W207 spring 2019 final project with Arthur Lima and Linda Dong',
     author='Yang Yang Qian',
+    author_email='yangyang.qian@berkeley.edu',
+    url='https://github.com/ucbiyyq/w207re-final-proj/',
     license='MIT',
 )
