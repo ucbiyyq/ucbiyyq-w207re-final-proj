6430d5c33ead4497d4257ab615536549b242d135
ucbiyyq <yangyang.qian@ischool.berkeley.edu>
2019-04-06 12:52:50 -0400

    added various scratch and stub for submission 1

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -72,8 +72,9 @@ class SFCCTransformer(BaseEstimator, TransformerMixin):
     See Chap 2 custom transformers
     
     """
-    def __init__(self, holiday_calendar = USFederalHolidayCalendar()):
+    def __init__(self, holiday_calendar = USFederalHolidayCalendar(), latitude_outlier = 50):
         self.holiday_calendar = holiday_calendar
+        self.latitude_outlier = latitude_outlier
         
     def fit(self, X, y = None):
         return self # no fitting
@@ -103,43 +104,48 @@ class SFCCTransformer(BaseEstimator, TransformerMixin):
             res = dt.isin(holidays).astype("int")
             return res
         
+        # creates a copy of the input dataframe
+        X_out = X.copy()
+        
         # extracts various date-related features
-        dtt = pd.to_datetime(X.Dates)
+        dtt = pd.to_datetime(X_out.Dates)
         
-        X["hour_delta"] = add_delta(dtt, "h") # hour since start, 0 to 108263
-        X["day_delta"] = add_delta(dtt, "D") # day since start, 0 to 4510
-        X["week_delta"] = add_delta(dtt, "W") # week since start, 0 to 644
-        X["month_delta"] = add_delta(dtt, "M") # month since start, 0 to 148
-        X["year_delta"] = add_delta(dtt, "Y") # year since start, 0 to 12
+        X_out["hour_delta"] = add_delta(dtt, "h") # hour since start, 0 to 108263
+        X_out["day_delta"] = add_delta(dtt, "D") # day since start, 0 to 4510
+        X_out["week_delta"] = add_delta(dtt, "W") # week since start, 0 to 644
+        X_out["month_delta"] = add_delta(dtt, "M") # month since start, 0 to 148
+        X_out["year_delta"] = add_delta(dtt, "Y") # year since start, 0 to 12
         
-        X["hour_of_day"] = dtt.dt.hour # 0 to 23
-        X["day_of_week"] = dtt.dt.dayofweek # 0 to 7, note day name is already DayOfWeek
-        X["day_of_month"] = dtt.dt.day # 1 to 31
-        X["day_of_year"] = dtt.dt.dayofyear # 1 to 365
-        X["week_of_year"] = dtt.dt.week # 2 to 52
-        X["month_of_year"] = dtt.dt.month # 1 to 12
-        X["quarter_of_year"] = dtt.dt.quarter # 1 to 4
-        X["year"] = dtt.dt.year # 2003 to 2015
+        X_out["hour_of_day"] = dtt.dt.hour # 0 to 23
+        X_out["day_of_week"] = dtt.dt.dayofweek # 0 to 7, note day name is already DayOfWeek
+        X_out["day_of_month"] = dtt.dt.day # 1 to 31
+        X_out["day_of_year"] = dtt.dt.dayofyear # 1 to 365
+        X_out["week_of_year"] = dtt.dt.week # 2 to 52
+        X_out["month_of_year"] = dtt.dt.month # 1 to 12
+        X_out["quarter_of_year"] = dtt.dt.quarter # 1 to 4
+        X_out["year"] = dtt.dt.year # 2003 to 2015
         
-        X["is_weekend"] = dtt.dt.dayofweek // 5 # 1 if sat or sun, 0 otherwise
-        X["is_holiday"] = calc_is_holiday(dtt) # 1 if holiday, 0 otherwise
+        X_out["is_weekend"] = dtt.dt.dayofweek // 5 # 1 if sat or sun, 0 otherwise
+        X_out["is_holiday"] = calc_is_holiday(dtt) # 1 if holiday, 0 otherwise
         
         # calculate cyclical values for hours, etc
         # http://blog.davidkaleko.com/feature-engineering-cyclical-features.html
-        X["hour_of_day_sin"] = np.round( np.sin(dtt.dt.hour * (2. * np.pi / 24)), 3)
-        X["hour_of_day_cos"] = np.round( np.cos(dtt.dt.hour * (2. * np.pi / 24)), 3)
+        X_out["hour_of_day_sin"] = np.round( np.sin(dtt.dt.hour * (2. * np.pi / 24)), 3)
+        X_out["hour_of_day_cos"] = np.round( np.cos(dtt.dt.hour * (2. * np.pi / 24)), 3)
         
-        X["day_of_week_sin"] = np.round( np.sin(dtt.dt.dayofweek * (2. * np.pi / 7)), 3)
-        X["day_of_week_cos"] = np.round( np.cos(dtt.dt.dayofweek * (2. * np.pi / 7)), 3)
+        X_out["day_of_week_sin"] = np.round( np.sin(dtt.dt.dayofweek * (2. * np.pi / 7)), 3)
+        X_out["day_of_week_cos"] = np.round( np.cos(dtt.dt.dayofweek * (2. * np.pi / 7)), 3)
         
-        X["month_of_year_sin"] = np.round( np.sin((dtt.dt.month - 1) * (2. * np.pi / 12)), 3)
-        X["month_of_year_cos"] = np.round( np.cos((dtt.dt.month - 1) * (2. * np.pi / 12)), 3)
+        X_out["month_of_year_sin"] = np.round( np.sin((dtt.dt.month - 1) * (2. * np.pi / 12)), 3)
+        X_out["month_of_year_cos"] = np.round( np.cos((dtt.dt.month - 1) * (2. * np.pi / 12)), 3)
         
         # TODO calculate police shifts? apparently its not regularly-spaced shifts
         
+        # TODO calculate address-based features, such as street, intersection, etc
         
+        # TODO calculate distance from hotspots of crime
         
-        return X
+        return X_out
 
 def print_summary(df):
     """
